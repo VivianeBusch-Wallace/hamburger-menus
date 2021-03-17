5962f661e2ecefa5c0950cfd48c5e7b96a80050b
viviane <v.buschwallace@yahoo.com>
2021-03-17 20:17:09 +0100

    added animation and transition to fourth and fifth hamburger menu

--- js/script.js
+++ js/script.js
@@ -21,5 +21,32 @@ document.querySelector(".menu-first").addEventListener("click", () => {
       "background-color: blue;";
     document.querySelector(".menu-first .circle9").style.cssText =
       "background-color: blue;";
+  } else {
+    document.querySelector(".menu-first .circle1").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle2").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle3").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle4").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle5").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle6").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle7").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle8").style.cssText =
+      "background-color: var(--light-gray);";
+    document.querySelector(".menu-first .circle9").style.cssText =
+      "background-color: var(--light-gray);";
   }
 });
+
+// document.querySelector(".menu-fourth").addEventListener("click", () => {
+//   clicked = !clicked;
+//   if (clicked) {
+//     document.querySelector(".menu-fourth").style.cssText =
+//       "background-color: rgb(238, 158, 101); transform: scale(1.3); transition-duration: 1s;";
+//   }
+// });
