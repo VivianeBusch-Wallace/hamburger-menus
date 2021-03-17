cf441142eaa0a1fd1b0a512dc4df755cf5e7628b
viviane <v.buschwallace@yahoo.com>
2021-03-17 15:39:18 +0100

    found mistake in js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 let clicked = false;
-        document.querySelector('.menu-first').addEventListener("click",()=>{
-        clicked= !clicked;
-            console.log(clicked);
-            if(clicked){
-                document.querySelector('.menu-first .bars').style.cssText = "background-color: blue; height: 2rem;
-                width: 2rem;
-                position: absolute;"
-            } 
-            
-            // else{
-            //     document.querySelector('').style.cssText = ""
-            // }
+document.querySelector(".menu-first").addEventListener("click", () => {
+  clicked = !clicked;
+  console.log(clicked);
+  if (clicked) {
+    document.querySelector(".menu-first .bars").style.cssText =
+      "background-color: blue; height: 2rem; width: 2rem; position: absolute;";
+  }
+});
