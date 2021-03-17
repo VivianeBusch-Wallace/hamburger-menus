ad96a9c0606c760c269f234dd296b9297939f9fe
viviane <v.buschwallace@yahoo.com>
2021-03-17 15:56:44 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ document.querySelector(".menu-first").addEventListener("click", () => {
   clicked = !clicked;
   console.log(clicked);
   if (clicked) {
-    document.querySelector(".menu-first .bars").style.cssText =
-      "background-color: blue; height: 2rem; width: 2rem; position: absolute;";
+    document.querySelector(".menu-first .circle1").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle2").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle3").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle4").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle5").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle6").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle7").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle8").style.cssText =
+      "background-color: blue;";
+    document.querySelector(".menu-first .circle9").style.cssText =
+      "background-color: blue;";
   }
 });
