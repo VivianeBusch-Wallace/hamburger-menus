89b78b037161c8b867d942e23df7668128410b43
viviane <v.buschwallace@yahoo.com>
2021-03-17 15:12:17 +0100

    added js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ let clicked = false;
         clicked= !clicked;
             console.log(clicked);
             if(clicked){
-                document.querySelector('.menu-first .bars').style.cssText = ""
-            } else{
-                document.querySelector('').style.cssText = ""
-            }
+                document.querySelector('.menu-first .bars').style.cssText = "background-color: blue; height: 2rem;
+                width: 2rem;
+                position: absolute;"
+            } 
+            
+            // else{
+            //     document.querySelector('').style.cssText = ""
+            // }
