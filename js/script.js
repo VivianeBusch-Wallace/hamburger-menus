3e70b7ac1ac142a30a944d44dc1f3ebb2070b336
viviane <v.buschwallace@yahoo.com>
2021-03-22 13:49:15 +0100

    added JS part for seventh menu

--- js/script.js
+++ js/script.js
@@ -71,3 +71,20 @@ document.querySelector(".menu-first").addEventListener("click", () => {
 //   }
 // });
 // wanna add keyframes here when I know how to do it
+
+// seventh menu:
+clicked = false;
+document.querySelector(".menu-seventh").addEventListener("click", () => {
+  clicked = !clicked;
+  if (clicked) {
+    document.querySelector(".menu-seventh .line1").style.cssText =
+      "transition: 1s; width: 10rem; height: 1rem; transform: rotate(45deg) translate(2.2rem, 2.4rem); background-color: chartreuse;";
+    document.querySelector(".menu-seventh .line3").style.cssText =
+      "transition: 1s; width: 10rem; height: 1rem; transform: rotate(-45deg) translate(2.2rem, -2.4rem); background-color: chartreuse;";
+  } else {
+    document.querySelector(".menu-seventh .line1").style.cssText =
+      "width: 7.5rem;height: 1rem;background-color: var(--light-gray);border-radius: 5rem;z-index: 2;";
+    document.querySelector(".menu-seventh .line3").style.cssText =
+      "width: 7.5rem;height: 1rem;background-color: var(--light-gray);border-radius: 5rem;z-index: 2;";
+  }
+});
